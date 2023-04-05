08d89d2768514b301b53dd1d557f46d4fffcbe4f
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-05 09:41:52 +0530

    [Rutuja]:Welcome

---
+++ Exception_Handlind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exception_Handlind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception Handling Practice Problem!");
+        }
+    }
+}
